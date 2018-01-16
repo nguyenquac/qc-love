6d4530ac00cec132be23a96cb7e9852eded2d51b
Anh Nguyen <nguyenquac@Anhs-MacBook-Pro.local>
2018-01-16 23:26:42 +0700

    Layout header

--- screens/Home.js
+++ screens/Home.js
@@ -33,6 +33,19 @@ class Home extends Component {
         let screenWidth = Dimensions.get('window').width;
         let headerImageRatio = 3/2;
         let headerImageHeight = screenWidth/headerImageRatio;
+        let avatarNamePanelHeight = headerImageHeight * 0.3;
+        let avatarRadius = avatarNamePanelHeight * 1.2;
+        let avatarNamePanelMarginTop = (avatarRadius - avatarNamePanelHeight)/2;
+
+        const avatarStyles = StyleSheet.create({
+            avatar: {
+                height: avatarRadius,
+                width: avatarRadius,
+                borderRadius: avatarRadius/2,
+                marginLeft: 5,
+                marginRight: 5,
+            },
+        });
 
         return (
             <View style={styles.container}>
@@ -49,7 +62,7 @@ class Home extends Component {
                 >
                 </Image>
                 <View style={{
-                    marginTop:headerImageHeight - 120,
+                    marginTop: headerImageHeight - avatarRadius - headerImageHeight*0.1,
                     backgroundColor: 'transparent',
                     flex: 1,
                     flexDirection: 'row',
@@ -57,17 +70,17 @@ class Home extends Component {
                     alignItems: 'center',
                 }}>
                     <View style={{
-                                    height:80,
+                                    height:avatarNamePanelHeight,
                                     backgroundColor: 'rgba(255, 255, 255, 0.8)',
                                     position: 'absolute',
-                                    top: 10,
+                                    top: avatarNamePanelMarginTop,
                                     left: 0,
                                     right: 0
                                 }}>
                     </View>
                     <Text style={styles.avatarName}>boy</Text>
-                    <Image source={{ uri: 'https://upload.wikimedia.org/wikipedia/en/1/17/Batman-BenAffleck.jpg'}} style={styles.avatar} />
-                    <Image source={{ uri: 'https://images.moviepilot.com/image/upload/c_fill,h_340,q_auto:good,w_460/b4yxwv7bffhfpc86clig.jpg'}} style={styles.avatar} />
+                    <Image source={{ uri: 'https://upload.wikimedia.org/wikipedia/en/1/17/Batman-BenAffleck.jpg'}} style={avatarStyles.avatar} />
+                    <Image source={{ uri: 'https://images.moviepilot.com/image/upload/c_fill,h_340,q_auto:good,w_460/b4yxwv7bffhfpc86clig.jpg'}} style={avatarStyles.avatar} />
                     <Text style={styles.avatarName}>girl</Text>
                 </View>
                 <View style={{
@@ -93,6 +106,7 @@ const styles = StyleSheet.create({
         height: 100,
         width: 100,
         borderRadius: 50,
+        margin: 5,
     },
     avatarName: {
         padding: 20,
